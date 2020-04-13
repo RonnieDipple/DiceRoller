c01cbdf2b7b125ed03246669a5ff349d018d7231
Ronnie <ronniedipple@gmail.com>
2020-04-13 17:36:14 -0500

    Added and update image resources

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,54 +1,43 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
-import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val resultText: TextView = findViewById(R.id.result_text)
+
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        rollButton.setOnClickListener { rollDice(resultText) }
 
-        countUpButton.setOnClickListener {
-            countUp(resultText)
-        }
+
+        rollButton.setOnClickListener { rollDice() }
+
     }
 
-    private fun rollDice(resultText: TextView){
+
+    private fun rollDice() {
 //        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val randomInt =(1..6).random()
-        resultText.text = randomInt.toString()
 
-    }
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
 
-    private fun countUp(resultText: TextView) {
-        if (resultText.text.toString() =="Hello World!") {
-            val num = 1
-            resultText.text = num.toString()
 
-        }else if (resultText.text.toString() =="6"){
-            return
         }
-
-        var myText = resultText.text.toString().toInt()
-        myText += 1
-        resultText.text = myText.toString()
+        dice_image.setImageResource(drawableResource)
 
 
     }
 
 
-
-
-    }
+}
 
