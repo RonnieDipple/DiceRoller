7181965fbdb67e37f4679ad751dc0d3d8c643043
Ronnie <ronniedipple@gmail.com>
2020-04-13 18:17:21 -0500

    challenge complete

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,12 +8,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
     lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image)
+
 
         rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -24,6 +27,13 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
 //        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
 
+        dice_image.setImageResource(getRandomDiceImage())
+        dice_image2.setImageResource(getRandomDiceImage())
+
+
+    }
+
+    private fun getRandomDiceImage(): Int {
         val randomInt = (1..6).random()
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
@@ -35,11 +45,9 @@ class MainActivity : AppCompatActivity() {
 
 
         }
-        dice_image.setImageResource(drawableResource)
+        return drawableResource
 
 
     }
-
-
 }
 
